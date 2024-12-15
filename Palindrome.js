4d6506ba0f021bd7d63630d233a96a549d1566f4
GMeghanaChowdary <167081845+GMeghanaChowdary@users.noreply.github.com>
2024-12-15 20:11:10 +0530

    Create Palindrome.js

---
+++ Palindrome.js
@@ -0,0 +1,5 @@
+function isPalindrome(str) {
+    str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    return str === str.split('').reverse().join('');
+}
+console.log(isPalindrome("A man a plan a canal Panama"));
