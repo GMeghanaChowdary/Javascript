6ece2ad6a6e33cbebf51d37805929221567a468c
GMeghanaChowdary <167081845+GMeghanaChowdary@users.noreply.github.com>
2024-12-15 20:22:02 +0530

    Create fetch API.js

---
+++ fetch API.js
@@ -0,0 +1,6 @@
+async function fetchData(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+}
+fetchData("https://jsonplaceholder.typicode.com/posts");
