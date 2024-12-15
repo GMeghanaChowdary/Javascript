adedbc60789d9785514660748892e69e2f175ab9
GMeghanaChowdary <167081845+GMeghanaChowdary@users.noreply.github.com>
2024-12-15 20:16:43 +0530

    Create anagram.js

---
+++ anagram.js
@@ -0,0 +1,5 @@
+function isAnagram(str1, str2) {
+    const normalize = str => str.toLowerCase().split('').sort().join('');
+    return normalize(str1) === normalize(str2);
+}
+console.log(isAnagram("listen", "silent"));
