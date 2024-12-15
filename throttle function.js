b7e41034c618b1473b3e5b8c33f6d10b96a6b885
GMeghanaChowdary <167081845+GMeghanaChowdary@users.noreply.github.com>
2024-12-15 20:17:50 +0530

    Create throttle function.js

---
+++ throttle function.js
@@ -0,0 +1,10 @@
+function throttle(func, limit) {
+    let inThrottle;
+    return function(...args) {
+        if (!inThrottle) {
+            func(...args);
+            inThrottle = true;
+            setTimeout(() => inThrottle = false, limit);
+        }
+    };
+}
